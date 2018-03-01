1b53ab350f02f763879657653f046abcecb553ce
HaijieZhao <zhaohaijie@gmail.com>
2018-03-01 19:56:28 +0000

    asdf

--- src/main/java/com/google/hashcode/hashimpl/TakeMeToDestinationGreedy.java
+++ src/main/java/com/google/hashcode/hashimpl/TakeMeToDestinationGreedy.java
@@ -1,35 +1,40 @@
 package com.google.hashcode.hashimpl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TakeMeToDestinationGreedy {
     private final ConcurrentLinkedQueue<Ride> rideList;
-    private final ConcurrentLinkedQueue<Vehicle> vehicles;
+    private final Map<Vehicle, ArrayList<Integer>> vehiclesRideMap;
     private final int simulationStep;
 
     public TakeMeToDestinationGreedy(DataLoader data){
-        rideList = new ConcurrentLinkedQueue(data.getRideList());
-        vehicles = new ConcurrentLinkedQueue(data.getVehicles());
+        rideList = new ConcurrentLinkedQueue();
+        vehiclesRideMap = new HashMap<Vehicle, ArrayList<Integer>>();
+        data.getVehicles().forEach(vehicle -> vehiclesRideMap.put(vehicle, new ArrayList<>()));
         simulationStep = data.getSimulationStep();
     }
 
 
     public void start() {
         for (int currentStep = 0; currentStep <= simulationStep; currentStep++) {
-            for (Vehicle vehicle : vehicles) {
+            for (Vehicle vehicle : vehiclesRideMap.keySet()) {
                 if (!vehicle.getOnDuty()) {
                     for (Ride ride : rideList) {
                         // It's already too late, we should remove it from the list and do nothing
                         // process next ride.
-                        if(TakeMeToDestinationHelper.alreadyTooLate(ride, currentStep)){
+                        if (TakeMeToDestinationHelper.alreadyTooLate(ride, currentStep)) {
                             rideList.remove(ride);
-                        }
-                        else{
-                            if(TakeMeToDestinationHelper.canArriveOnTime(vehicle, ride, currentStep, simulationStep)){
+                        } else {
+                            if (TakeMeToDestinationHelper.canArriveOnTime(vehicle, ride, currentStep, simulationStep)) {
                                 vehicle.setOnDuty(true);
                             }
                         }
                     }
+                } else {
+
                 }
             }
         }
