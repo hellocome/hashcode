bbdfec2038e9ff4d424ab7c5aead3314b1225921
HaijieZhao <zhaohaijie@gmail.com>
2018-03-01 21:10:35 +0000

    D

--- src/main/java/com/google/hashcode/HashApp.java
+++ src/main/java/com/google/hashcode/HashApp.java
@@ -14,8 +14,8 @@ public class HashApp {
             final String result = "E:\\SourceControl\\MyGitHub\\hashcode\\result\\e";
             //System.setProperty(HASHCODE_FILE, "E:\\SourceControl\\MyGitHub\\hashcode\\DataSet\\b_should_be_easy.in");
             //System.setProperty(HASHCODE_FILE, "E:\\SourceControl\\MyGitHub\\hashcode\\DataSet\\c_no_hurry.in");
-            //System.setProperty(HASHCODE_FILE, "E:\\SourceControl\\MyGitHub\\hashcode\\DataSet\\d_metropolis.in");
-            System.setProperty(HASHCODE_FILE, "E:\\SourceControl\\MyGitHub\\hashcode\\DataSet\\a_example.in");
+            System.setProperty(HASHCODE_FILE, "E:\\SourceControl\\MyGitHub\\hashcode\\DataSet\\d_metropolis.in");
+            //System.setProperty(HASHCODE_FILE, "E:\\SourceControl\\MyGitHub\\hashcode\\DataSet\\a_example.in");
 
             for(int i=0; i<100; i++) {
                 DataLoader loader = new DataLoader(System.getProperty(HASHCODE_FILE));
